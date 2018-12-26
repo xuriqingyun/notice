935fe96dcc5c359bed90bf4b5b9cc7f60326a99e
zhouxu <zhouxu@pioneerservice.cn>
2018-12-26 13:59:05 +0800

    sms工具调整

--- src/main/java/zhouxu/site/notice/utils/SmsUtils.java
+++ src/main/java/zhouxu/site/notice/utils/SmsUtils.java
@@ -1,9 +1,6 @@
 package zhouxu.site.notice.utils;
 
-import org.apache.commons.httpclient.Header;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.NameValuePair;
-import org.apache.commons.httpclient.methods.PostMethod;
+import zhouxu.site.notice.exception.BizException;
 
 import java.io.IOException;
 
@@ -29,19 +26,32 @@ public class SmsUtils {
      * @throws
      **/
     public static boolean send(String phone,String content) throws IOException {
-        HttpClient client = new HttpClient();
-        PostMethod post = new PostMethod("http://gbk.api.smschinese.cn");
-        post.addRequestHeader("Content-Type","application/x-www-form-urlencoded;charset=gbk");//在头文件中设置转码
-        NameValuePair[] data ={ new NameValuePair("Uid", UID),new NameValuePair("Key", KEY),new NameValuePair("smsMob",phone),new NameValuePair("smsText",content)};
-        post.setRequestBody(data);
-        client.executeMethod(post);
-        Header[] headers = post.getResponseHeaders();
-        int statusCode = post.getStatusCode();
-        String result = new String(post.getResponseBodyAsString().getBytes("gbk"));
-        post.releaseConnection();
-        if("1".equals(result)){
+//        HttpClient client = new HttpClient();
+//        PostMethod post = new PostMethod("http://gbk.api.smschinese.cn");
+//        post.addRequestHeader("Content-Type","application/x-www-form-urlencoded;charset=gbk");//在头文件中设置转码
+//        NameValuePair[] data ={ new NameValuePair("Uid", UID),new NameValuePair("Key", KEY),new NameValuePair("smsMob",phone),new NameValuePair("smsText",content)};
+//        post.setRequestBody(data);
+//        client.executeMethod(post);
+//        Header[] headers = post.getResponseHeaders();
+//        int statusCode = post.getStatusCode();
+//        String result = new String(post.getResponseBodyAsString().getBytes("gbk"));
+//        post.releaseConnection();
+//        if(result==null||"".equals(result)){
+//            throw new BizException("unkown");
+//        }else{
+//            if(Integer.parseInt(result)>0){
+//                return true;
+//            }else{
+//                throw new BizException(result);
+//            }
+//        }
+        HttpClientUtil client = HttpClientUtil.getInstance();
+        //UTF发送
+        int result = client.sendMsgUtf8(UID, KEY, content, phone);
+        if(result>0){
             return true;
+        }else{
+            throw new BizException(client.getErrorMsg(result));
         }
-        return false;
     }
 }
